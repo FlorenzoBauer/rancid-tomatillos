f1ae327c914fcb544c6b2b0174017cd7797bbc62
Florenzo Bauer <florenzobauer@gmail.com>
2024-02-05 18:17:15 -0700

    Single movie API for Movie Details

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,11 +1,25 @@
 import './MovieDetails.css'
+import { useState, useEffect } from 'react'
 import Header from '../Header/Header.js'
 
-const GetMovieId = ({ movies, selectedMovieId }) => {
-  const movie = movies.find(movie => movie.id === selectedMovieId);
+const GetMovieId = ({ selectedMovieId }) => {
+const [movie, setMovie] = useState(null);
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${selectedMovieId}`);
+      const data = await response.json();
+      setMovie(data.movie);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  if (!movie) return null;
-
+  fetchData();
+}, [selectedMovieId]);
+        console.log("hello", movie)
+        //movie.genre.forEach(genre
+  if (!movie) return (<h1>Error Loading...</h1>);
   return (
     <div className="movie-details-container">
       <div className="backdrop">
@@ -14,6 +28,8 @@ const GetMovieId = ({ movies, selectedMovieId }) => {
       <div className="individual-movie">
         <h2>{movie.title}</h2>
         <p><strong>Overview:</strong> {movie.overview}</p>
+        <p><strong>Genres:</strong> {movie.genres}</p>
+        <p><strong></strong>Run Time: {movie.runtime}</p>
         <p><strong>Release Date:</strong> {movie.release_date}</p>
         <p><strong>Average Rating:</strong> {movie.average_rating}</p>
       </div>
