ebfc123d3fad3a501c4ca1ba6be389c30d710e4a
Jamie Francisco <0jamiefrancisco@gmail.com>
2024-02-05 16:54:23 -0800

    Add more styling to movie details page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,11 +3,12 @@ import './Header.css';
 const Header = ({selectedMovieId, setSelectedMovieId}) => {
     return (
         <header className="title">
-        <h1>Rancid Tomatillos</h1>
-        {selectedMovieId ? (
-            <button className="homeButton" onClick={() => setSelectedMovieId(null)}>Back to Movies</button>
-        ) : null}
+            <h1>Rancid Tomatillos</h1>
+            {selectedMovieId && (
+                <button className="homeButton" onClick={() => setSelectedMovieId(null)}>Back to Movies</button>
+            )}
         </header>
     );
-    }
+}
+
 export default Header;
